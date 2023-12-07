40cae953fe4c7b1ce6a62b5da2b297d7997e4fbe
vickyhwang77 <67029487+vickyhwang77@users.noreply.github.com>
2023-12-06 23:11:13 -0500

    can login/signup with google

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Signup from "./Signup"
 import Signup2 from "./Signup2"
+import Signup2Goog from "./Signup2Goog"
+import GoogleRegistration from './GoogleRegister'
 import Login from './Login'
 import Home from './Home'
+import GoogleLogin from './LoginGoogle'
+
 
 
 function App() {
@@ -15,9 +19,13 @@ function App() {
 
         <Route path="/register" element={<Signup />}></Route>
         <Route path="/register2" element={<Signup2 />}></Route>
+        <Route path="/register2google" element={<Signup2Goog />}></Route>
         <Route path="/login" element={<Login />}></Route>
+        <Route path="/googlelogin" element={<GoogleLogin />}></Route>
         <Route path="/home" element={<Home />}></Route>
         
+
+        <Route path="/registerwithgoogle" element={<GoogleRegistration />}></Route>
       </Routes>
    </BrowserRouter>
   )
