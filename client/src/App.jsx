1fec87f6ce4a355492b6cea05c3c4215374e0fc5
vickyhwang77 <67029487+vickyhwang77@users.noreply.github.com>
2023-12-08 11:15:09 -0500

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,6 @@ import { useState, useEffect } from 'react'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Signup from "./Signup"
-import Signup2 from "./Signup2"
-import Signup2Goog from "./Signup2Goog"
 import GoogleRegistration from './GoogleRegister'
 import Login from './Login'
 import Home from './Home'
@@ -18,8 +16,6 @@ function App() {
       <Routes>
 
         <Route path="/register" element={<Signup />}></Route>
-        <Route path="/register2" element={<Signup2 />}></Route>
-        <Route path="/register2google" element={<Signup2Goog />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/googlelogin" element={<GoogleLogin />}></Route>
         <Route path="/home" element={<Home />}></Route>
